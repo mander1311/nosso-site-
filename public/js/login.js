6b8a22b05fe3b55b87d3dfffd24855b53ae5eceb
mander1311 <150467335+mander1311@users.noreply.github.com>
2026-06-02 23:42:17 +0000

    Adiciona tela de login com senha

--- public/js/login.js
+++ public/js/login.js
@@ -1,39 +1,11 @@
-async function entrar(){
+function entrar(){
 
 const senha =
 document.getElementById("senha")
 .value
 .trim();
 
-const encoder =
-new TextEncoder();
-
-const data =
-encoder.encode(senha);
-
-const hashBuffer =
-await crypto.subtle.digest(
-"SHA-256",
-data
-);
-
-const hashArray =
-Array.from(
-new Uint8Array(hashBuffer)
-);
-
-const hash =
-hashArray
-.map(b =>
-b.toString(16)
-.padStart(2,"0")
-)
-.join("");
-
-const hashCorreto =
-"2a6b3ee0c7b0f8a6e2d2c5df0fbc4a4eb7fd3fd8261eb998f8a0b6a5e0b80d70";
-
-if(hash === hashCorreto){
+if(senha === "130311"){
 
 window.location.href =
 "home.html";
