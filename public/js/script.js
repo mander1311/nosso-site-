7b501a7132b3a79c2b2d11cf8029182a01c1c82a
mander1311 <150467335+mander1311@users.noreply.github.com>
2026-06-02 22:53:29 +0000

    Melhora design do site da Laura

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,38 @@
 const contador = document.getElementById("contador");
 
-const inicio = new Date("2025-04-09T15:20:00");
+const inicio = new Date("2026-04-09T15:20:00");
 
-function atualizar() {
+function atualizar(){
 
 const agora = new Date();
 
-const diferenca = agora - inicio;
+const diff = agora - inicio;
 
-const dias = Math.floor(diferenca / (1000*60*60*24));
+const dias =
+Math.floor(diff/(1000*60*60*24));
 
-const horas = Math.floor(
-(diferenca % (1000*60*60*24))
+const horas =
+Math.floor(
+(diff%(1000*60*60*24))
 /
 (1000*60*60)
 );
 
-const minutos = Math.floor(
-(diferenca % (1000*60*60))
+const minutos =
+Math.floor(
+(diff%(1000*60*60))
 /
 (1000*60)
 );
 
-const segundos = Math.floor(
-(diferenca % (1000*60))
+const segundos =
+Math.floor(
+(diff%(1000*60))
 /
 1000
 );
 
-contador.innerHTML =
+contador.innerHTML=
 `
 ❤️ ${dias} dias<br>
 ❤️ ${horas} horas<br>
@@ -38,4 +42,5 @@ contador.innerHTML =
 }
 
 setInterval(atualizar,1000);
+
 atualizar();
